1895dffffacfdd795b3cc432b92b30b2955f2007
martinapetrova <martina.petrova.779@gmail.com>
2015-08-18 18:00:22 +0300

    Styling the shopping cart

--- OnlineWatchShop/scripts/shopping-cart.js
+++ OnlineWatchShop/scripts/shopping-cart.js
@@ -1,5 +1,6 @@
 ﻿/// <reference path="C:\Users\MARTY\Desktop\OnlineWatchShop\OnlineWatchShop\libs/jquery-2.1.1.js" />
 
+
 setTimeout(function () {
     $('.shopping-cart').css('display', 'inline');
 }, 1000);
